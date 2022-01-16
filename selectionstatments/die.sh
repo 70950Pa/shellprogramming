0455ed340cf5291a53124d834a24acb703d41f70
70950Pa <97427545+70950Pa@users.noreply.github.com>
2022-01-16 10:41:01 +0530

    Added: roll of a die

---
+++ selectionstatments/die.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+num=1;
+num=2;
+num=3;
+num=4;
+num=5;
+num=6;
+randomNum=$((  (RANDOM%6) + 1));
+echo $randomNum
+
---
+++ selelctionstatments/empWage.sh
@@ -0,0 +1,25 @@
+#!bin/bash -x
+isFullTime=1;
+isPartTime=2;
+totalSalary=0;
+empPerHrs=50;
+numWorkingDays=10;
+for (( $day=1; $day<=numWorkingDays; $day=day++ ));
+do
+   empCheck=$(( RANDOM%3 ));
+   case $empCheck
+      1)
+         empHrs=8;
+         ;;
+      2)
+         empHrs=4;
+         ;;
+      *)
+         empHrs=0;
+         ;;
+   esac
+   salary=$(( $empPerHrs * $empHrs ));
+   totalSalary=$(( $totalSalary + $salary ));
+done
+echo $totalSalary
+
