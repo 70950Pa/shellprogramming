c7af10f610f45e1c656a9b6fc59c58b3492279ae
70950Pa <97427545+70950Pa@users.noreply.github.com>
2022-01-18 10:54:48 +0530

    Delete addAvg.sh

--- addAvg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-read -p "enter the forst number :" num1;
-read -p "enter second number :" num2;
-read -p "enter third number :" num3;
-read -p "enter fourth number :" num4;
-read -p "enter fifth number :" num5;
- rondomCheck=$(( (( RANDOM%5 )) +1 ));
-
