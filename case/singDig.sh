b4c19b095cd996bf85a5b4f03af1a54ed5b7a8ec
70950Pa <97427545+70950Pa@users.noreply.github.com>
2022-01-19 17:57:28 +0530

    Added: singleDigit

---
+++ case/singDig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+read num
+
+case $num in
+	1)
+	echo "One"
+	;;
+	2)
+	echo "Two"
+	;;
+	3)
+	echo "Three"
+ 	;;
+	4)
+	echo "Four"
+	;;
+	*)
+	echo "enter single digit num"
+	;;
+esac
+
